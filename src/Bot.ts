b85d0779847c19500272f2247c8dcc3ff1defd6a
Renan Pinheiro <renancamerapinheiro@gmail.com>
2022-08-21 19:27:02 -0300

    feat: add ready listener

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,10 +1,13 @@
 import "dotenv/config";
-import { Client } from "discord.js";
+import { Client, ClientOptions } from "discord.js";
+import ready from "./listeners/ready";
 
 console.log("Bot is starting...");
 
 const client = new Client({
   intents: []
 });
+
+ready(client);
 
 client.login(process.env.BOT_TOKEN);
